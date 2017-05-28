ac8728656fe3d35ea4b24d377416d47ae3221b24
Ondrej Mihalyi <ondrej.mihalyi@gmail.com>
2017-05-28 11:55:00 +0200

    Caching with Payara Server Don't forget to enable Hazelcast. Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@gmail.com>

---
+++ 03-payara/src/main/java/mihalyi/scaling/payara/CachedScheduleDAO.java
@@ -0,0 +1,62 @@
+/** **************************************************
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ *************************************************** */
+package mihalyi.scaling.payara;
+
+import io.microprofile.showcase.bootstrap.BootstrapData;
+import io.microprofile.showcase.schedule.model.Schedule;
+import io.microprofile.showcase.schedule.persistence.ScheduleDAO;
+import java.util.*;
+import java.util.stream.StreamSupport;
+import javax.cache.Cache;
+import javax.cache.Caching;
+import javax.cache.configuration.MutableConfiguration;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Specializes;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Ondrej Mihalyi
+ */
+@Specializes
+@ApplicationScoped
+public class CachedScheduleDAO extends ScheduleDAO {
+
+    Cache<String, Schedule> cachedSchedules;
+
+    public CachedScheduleDAO() {
+    }
+
+    @Inject
+    public CachedScheduleDAO(BootstrapData bootstrapData) {
+        super(bootstrapData);
+        MutableConfiguration<String, Schedule> config = new MutableConfiguration<>();
+        cachedSchedules = Caching.getCachingProvider().getCacheManager()
+                .createCache("cachedSchedules", config);
+    }
+
+    @Override
+    public List<Schedule> getAllSchedules(int limit) {
+        List<Schedule> result = getAllSchedulesFromCache(limit);
+        if (result.size() < limit) {
+            result = super.getAllSchedules(limit);
+            result.stream().forEach(schedule -> {
+                cachedSchedules.put(schedule.getId(), schedule);
+            });
+        }
+        return result;
+    }
+
+    private ArrayList<Schedule> getAllSchedulesFromCache(int limit) {
+        ArrayList<Schedule> result = new ArrayList<>();
+        StreamSupport.stream(cachedSchedules.spliterator(), false)
+                .limit(limit)
+                .map(entry -> entry.getValue())
+                .forEach(result::add);
+        return result;
+    }
+
+}
